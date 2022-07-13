c727e9022ec717956dc163dc16a4b866300c0196
son <jhson0507@gmail.com>
2022-07-13 10:56:01 +0900

    ~p/151 게시글 List 표출

--- src/main/java/com/jhson/book/springboot/domain/posts/PostsRepositroy.java
+++ src/main/java/com/jhson/book/springboot/domain/posts/PostsRepositroy.java
@@ -1,7 +1,13 @@
 package com.jhson.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepositroy extends JpaRepository<Posts, Long> {
-
+    
+    //SpringDataJap에서 제공하지 않는 메소드는 아래처럼 쿼리 작성도 가능함
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jhson/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jhson/book/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.jhson.book.springboot.service.posts;
 
 import com.jhson.book.springboot.domain.posts.Posts;
 import com.jhson.book.springboot.domain.posts.PostsRepositroy;
+import com.jhson.book.springboot.web.dto.PostsListResponseDto;
 import com.jhson.book.springboot.web.dto.PostsResponseDto;
 import com.jhson.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jhson.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepositroy.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepositroy.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
